a4d95d1ea4aa063cc8d0a16389f8bb46bc75b2fe
alevlez23 <evelez7746@utm.edu.ec>
2025-12-30 14:49:37 -0500

    Servir frontend desde backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const conectarDB = require("./config/db");
 
 const app = express();
+
+// Conectar MongoDB
 conectarDB();
 
 /* ===== MIDDLEWARE ===== */
@@ -18,9 +21,15 @@ app.use("/api/productos", require("./routes/productos"));
 app.use("/api/movimientos", require("./routes/movimientos"));
 app.use("/api/auth", require("./routes/auth"));
 
+/* ===== SERVIR FRONTEND REACT ===== */
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 /* ===== SERVIDOR ESCUCHANDO EN TODAS LAS IPs ===== */
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
