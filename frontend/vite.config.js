25565a2212bb9a30f5e0d22e00d497ba59673843
Alejandra Velez <evelez7746@utm.edu.ec>
2026-08-20 17:13:25 -0500

    Change base path in Vite config
    Update base path for deployment to 'Ferreteria_Alejandra'.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Mantiene rutas absolutas correctas para deploy
+  base: "/Ferreteria_Alejandra/",
 });
